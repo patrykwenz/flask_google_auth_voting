b98062a9d41c43126d12e8b8975fca960081f81b
root <root@ninjait.pl>
2020-11-16 15:57:25 +0100

    Typo

--- launch-server.sh
+++ launch-server.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Script launches Flask application on production WSGI server for nginx reverse proxy
-gunicorn --bind=unix:/tmp/gunicorn.sock --workers=8 myapp:app
+gunicorn --bind=unix:/tmp/gunicorn.sock --workers=4 app:app
