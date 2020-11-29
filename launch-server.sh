0b198bf6ef7547a2cacb79180c8808cd772ce02a
root <root@ninjait.pl>
2020-11-29 21:35:02 +0100

    Created a systemd service to run the server 24/7

--- launch-server.sh
+++ launch-server.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 # Script launches Flask application (through CherryPy) on production WSGI server for nginx reverse proxy
+cd /var/www/evoting/
 gunicorn --bind=unix:/tmp/gunicorn.sock --workers=1 cherry-app:app
